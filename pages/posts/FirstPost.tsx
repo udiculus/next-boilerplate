b9c8d9424d2f671ae071666ca8f299cef2a4d326
Yudhi Guntara <yudhi.guntara@gmail.com>
2022-12-01 18:57:12 +0700

    Added sample of custom page

---
+++ pages/posts/FirstPost.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FirstPost = () => {
+  return (
+    <>
+      <button>hello</button>
+      <h1>Hello World!</h1>
+    </>
+  );
+};
+
+export default FirstPost;
